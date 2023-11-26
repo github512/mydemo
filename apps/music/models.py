358fb39f7909a41f8d048e286bdc6e1340f98b22
demo1 <123@123.com>
2023-11-26 22:59:16 +0800

    需求：建立一些ForeignKey與ManyToMany關係 更新内容： refactor(重構): 1、在apps.music.models下面新建Author和Book两张表。                 2、Author表的book字段关联了book表，当book表内某个数据删除的时候，Author表下的book字段关联的数据也会被删除。                 3、Book表里面添加author字段，和Author表建立多对多的关系

--- apps/music/models.py
+++ apps/music/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+from apps.clients.models import Musician
+
+
+
+
+class Author(models.Model):
+    name = models.CharField(max_length=64)
+    book = models.ForeignKey("Book", on_delete=models.CASCADE)
+    def __unicode__(self):
+    	return self.name
+
+
+class Book(models.Model):
+	name = models.CharField(max_length=64)
+	author = models.ManyToManyField(Author)
+	def __unicode__(self):
+		return self.name
+
+
+
