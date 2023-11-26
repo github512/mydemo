370bdafc9b10a33c751421ec9dd7439f674b7fc6
demo1 <123@123.com>
2023-11-26 10:36:02 +0800

    需求： 1、增建其他APPS 更新内容 1、调整APPS目录架构，增加music、clients、users 2、调整templastes路径，统一放在各个APP下面

---
+++ apps/clients/templates/js/nav_left.js
@@ -0,0 +1,11 @@
+	$(document).ready(
+	function() 
+	{
+		$(".menuTitle3").click(function(){
+			$(this).next("div").slideToggle("slow")
+			.siblings(".menuContent3:visible").slideUp("slow");
+			$(this).toggleClass("activeTitle3");
+			$(this).siblings(".activeTitle3").removeClass("activeTitle3");
+		});
+	});
+
---
+++ apps/clients/views.py
@@ -0,0 +1,40 @@
+from django.shortcuts import render
+
+import datetime
+
+from apps.clients.models import Musician
+
+# Create your views here.
+
+from django.http import HttpResponse, JsonResponse
+
+# ================================================= #
+# 1. Insert statement
+# ================================================= #
+
+
+def index(request):
+    now = datetime.datetime.now()
+    html = "<html><body>It is now %s.</body></html>" % now
+    p=Musician.objects.create(first_name="张三",last_name='20231111',instrument='10')
+    p.save()
+    qs=Musician.objects.values()
+    retlist = list(qs)
+
+    print(retlist)
+    return HttpResponse(html)
+    #return JsonResponse({'ret': 0, 'retlist': retlist})
+
+
+# ================================================= #
+# 1. Pass variables to the page
+# ================================================= #
+
+
+def html(request):
+    qs=Musician.objects.values()
+    retlist = list(qs)
+    a = {'name': retlist }  
+    return render(request, 'index.html',a)
+
+
